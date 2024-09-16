d3ada518005792957fbe42fffcb2d754e93687ac
04amanrajj <04.aman.raj@gmail.com>
2024-09-16 23:32:45 +0530

    utils reusable function

---
+++ utils/utils.js
@@ -0,0 +1,67 @@
+const filter = async (option, data, type) => {
+  // console.log({ option, data, type });
+
+  let newData = data.filter((element) => {
+    if (element[type]) {
+      return element[type].toLowerCase().includes(option.toLowerCase());
+    }
+  });
+  return newData;
+};
+
+const tostTopEnd = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: false,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
+
+const tostBottomEnd = Swal.mixin({
+  toast: true,
+  position: "bottom-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
+
+// authantication
+function isUserLoggedin() {
+  document.addEventListener("DOMContentLoaded", function () {
+    let loggedInUser = localStorage.getItem("loggedInUser");
+    let savedUser = localStorage.getItem("loggedInUser");
+    // console.log(savedUser);
+    if (savedUser != null) {
+      isUserLoggedin();
+      tostTopEnd.fire({
+        icon: "success",
+        title: "Logged in successfully",
+      });
+    }
+
+    if (loggedInUser) {
+      let loginLink = document.querySelector(".user");
+      if (loginLink) {
+        loginLink.textContent = loggedInUser;
+        loginLink.style.display="flex"
+      }
+    }
+  });
+}
+// redirect to shop
+function redirect() {
+  let input = document.querySelector("#product");
+  input.addEventListener("input", () => {
+    window.location.href = "shop.html";
+  });
+}
+
+export { tostTopEnd, tostBottomEnd, filter, isUserLoggedin, redirect };
